78425842054341207293ce23a48019baaf369fcd
Zheng Cai <ashlike94@hotmail.com>
2024-04-03 16:34:01 +0800

    added few fixes

--- app.js
+++ app.js
@@ -41,22 +41,12 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static('./public'));
 app.use(fileUpload());
 
-app.get("/", (req, res) => {
-  res.send("e-commerce api");
-});
-
-app.get("/api/v1", (req, res) => {
-  //   console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("e-commerce api");
-});
 app.use("/api/v1/auth", authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
